de9cdd7a9f05aa010e76c6a7c1579e603b147adb
Rsibaja19 <45179513+Rsibaja19@users.noreply.github.com>
2022-03-02 21:15:30 -0500

    Add files via upload

--- actividad_py.py
+++ actividad_py.py
@@ -4,5 +4,12 @@ Spyder Editor
 
 This is a temporary script file.
 """
-donar = input('Digite el valor de su donacion')
-
+donar = int(input("Digite el valor de su donacion"))
+tele = donar * 0.10
+sistemas = donar * 0.25
+administracion = donar * 0.35
+contabilidad = donar * 0.30
+print(f' el valor correspondiente a la donacion es:  {tele:,}')
+print(f' el valor correspondiente a la donacion es:  {sistemas:,}')
+print(f' el valor correspondiente a la donacion es:  {administracion:,}')
+print(f' el valor correspondiente a la donacion es:  {contabilidad:,}')
